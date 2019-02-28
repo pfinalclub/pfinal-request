e66995ab28b2ca35ea068b8e181439509240be4b
南丞 <Lampxiezi@163.com>
2019-02-28 17:30:12 +0800

    更新

--- src/build/Base.php
+++ src/build/Base.php
@@ -39,7 +39,24 @@ class Base
 
     public function __construct()
     {
-
+        $_SERVER['SCRIPT_NAME'] = str_replace('\\', '/', $_SERVER['SCRIPT_NAME']);
+        if (!isset($_SERVER['REQUEST_METHOD'])) {
+            $_SERVER['REQUEST_METHOD'] = '';
+        }
+        if (!isset($_SERVER['HTTP_HOST'])) {
+            $_SERVER['HTTP_HOST'] = '';
+        }
+        if (!isset($_SERVER['REQUEST_URI'])) {
+            $_SERVER['REQUEST_URI'] = '';
+        }
+        defined('NOW') or define('NOW', $_SERVER['REQUEST_TIME']);
+        defined('MICROTIME') or define('MICROTIME', $_SERVER['REQUEST_TIME_FLOAT']);
+        defined('__URL__') or define('__URL__', $this->url());
+        defined('__HISTORY__') or define("__HISTORY__", $this->history());
+        defined('__ROOT__') or define('__ROOT__', $this->domain());
+        defined('__WEB__') or define('__WEB__', $this->web());
+        define('DS', DIRECTORY_SEPARATOR);
+        $this->defineRequestConst();
     }
 
     public function url()
@@ -56,4 +73,52 @@ class Base
             return true;
         }
     }
+
+    public function __call($action, $arguments)
+    {
+        $action = strtoupper($action);
+        if (empty($arguments)) {
+            return $this->items[$action];
+        }
+        $data = PFarr::pf_get($this->items[$action], $arguments[0]);
+        if (!is_null($data) && !empty($arguments[2])) {
+            return $this->batchFunctions($arguments[2], $data);
+        }
+        return !is_null($data) ? $data : (isset($arguments[1]) ? $arguments[1] : null);
+    }
+
+
+    protected function batchFunctions($functions, $value)
+    {
+        $functions = is_array($functions) ? $functions : [$functions];
+        foreach ($functions as $func) {
+            $value = $func($value);
+        }
+        return $value;
+    }
+
+    protected function defineRequestConst()
+    {
+        $this->items['POST']    = $_POST;
+        $this->items['GET']     = $_GET;
+        $this->items['REQUEST'] = $_REQUEST;
+        $this->items['SERVER']  = $_SERVER;
+        $this->items['GLOBALS'] = $GLOBALS;
+        //$this->items['SESSION'] = Session::all();
+        $this->items['COOKIE']  = Cookie::all();
+        if ( empty( $_POST ) ) {
+            $input = file_get_contents( 'php://input' );
+            if ( $data = json_decode( $input, true ) ) {
+                $this->items['POST'] = $data;
+            }
+        }
+        defined( 'IS_GET' ) or define( 'IS_GET', $this->isMethod( 'get' ) );
+        defined( 'IS_POST' ) or define( 'IS_POST', $this->isMethod( 'post' ) );
+        defined( 'IS_DELETE' ) or define( 'IS_DELETE', $this->isMethod( 'delete' ) );
+        defined( 'IS_PUT' ) or define( 'IS_PUT', $this->isMethod( 'put' ) );
+        defined( 'IS_AJAX' ) or define( 'IS_AJAX', $this->isAjax() );
+        defined( 'IS_WECHAT' ) or define( 'IS_WECHAT', $this->isWeChat() );
+        defined( 'IS_MOBILE' ) or define( 'IS_MOBILE', $this->isMobile() );
+    }
+
 }
